1a9ad159521b49e804097363dcfa0dde4949c546
Celeste Moore <celestealexmoore@gmail.com>
2022-05-21 11:15:40 -0500

    random password renders in console

--- assets/script.js
+++ assets/script.js
@@ -1,66 +1,88 @@
 // Assignment Code
 let generateBtn = document.getElementById("generate");
-let capChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-let lowChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-let numChar = ["1","2","3","4","5","6","7","8","9","0"];
-let specialChar = ["!","#","-","_","~","."];
+let capChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowChar = "abcdefghijklmnopqrstuvwxyz";
+let numChar = "1234567890";
+let specialChar = "!#-_~.";
+let passwordChars = '';
 
-// Write password to the #password input
+
 function writePassword() {
-  // let password = generatePassword();
-  // let passwordText = document.getElementById("#password");
-
-  // passwordText.value = password;
-
   let lengthConfirm = prompt("How many characters would you like your password to contain?", "Please enter a number between 8 and 128.")
   verifyAnswer(lengthConfirm);
-  console.log(lengthConfirm);
+  // console.log(lengthConfirm);
 
   function verifyAnswer(input) {
     if(input < 8 || input > 128 || isNaN(input)){
       let newLengthConfirm = prompt("How many characters would you like your password to contain?", "Please enter a number between 8 and 128.")
       writePassword(newLengthConfirm);
-      console.log(newLengthConfirm);
+      // console.log(newLengthConfirm);
     };
   }
-
-    //I want this to run again.
 
   let capConfirm = confirm("Do you want your password to contain capital letters?");
     if(capConfirm === true) {
       alert("Your password will include capital letters.")
-      console.log("Include capital letters");
     }else{
       alert("Your password will not include capital letters.")
-      console.log("Do not include capital letters");
     }
   let lowConfirm = confirm("Do you want your password to contain lowercase letters?");
     if(lowConfirm === true) {
       alert("Your password will include lowercase letters.")
-      console.log("Include lowecase letters");
     }else{
       alert("Your password will not include lowercase letters.")
-      console.log("Do not include lowercase letters");
     }
   let numConfirm = confirm("Do you want your password to contain numbers?");
     if(numConfirm === true) {
       alert("Your password will include numbers.")
-      console.log("Include numbers");
     }else{
       alert("Your password will not include numbers.")
-      console.log("Do not include numbers");
     }
   let charConfirm = confirm("Do you want your password to contain special characters?");
     if(charConfirm === true) {
       alert("Your password will include special characters.")
-      console.log("Include special characters");
     }else{
       alert("Your password will not include special characters.")
-      console.log("Do not include special characters");
     }
 
+    function createJumboString(capital, lowercase, numbers, characters){
+      if(capital === true){
+        passwordChars += capChar;
+      }
+      if(lowercase === true){
+        passwordChars += (lowChar);
+      }
+      if(numbers === true){
+        passwordChars += (numChar);
+      }
+      if(characters === true){
+        passwordChars += (specialChar);
+      }
+    }
+    createJumboString(capConfirm, lowConfirm, numConfirm, charConfirm);
 
+    function generatePassword(max) {
+      // jumble it up and only give me 16 chars back.
+      let password = '';
+   
+      for(let i = 0; i < max; i++) {
+        password += passwordChars.charAt(Math.floor(Math.random() * passwordChars.length));
+        console.log(password);
+      }
+    }
+   generatePassword(lengthConfirm);
 }
 
 // Add event listener to generate button
 generateBtn.onclick = writePassword();
+ // Write password to the #password input
+    // let password = generatePassword();
+    // let passwordText = document.getElementById("#password");
+    // passwordText.value = password;
+
+
+/* 
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+*/
